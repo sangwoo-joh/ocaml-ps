4631ad0349da587e2988277bac9f598186a1bc3d
Sangwoo Joh <lambdasnow@gmail.com>
2019-08-23 11:09:27 +0900

    super optimization

--- 1157/main.ml
+++ 1157/main.ml
@@ -1,28 +1,28 @@
-let make_hashtbl str =
+let make_array str =
   let char_seq = Seq.map Char.uppercase_ascii (String.to_seq str) in
-  let hashtbl = Hashtbl.create 33 in
-  Seq.iter
-    (fun char ->
-      match Hashtbl.find_opt hashtbl char with
-      | None -> Hashtbl.add hashtbl char Int32.one
-      | Some cnt -> Hashtbl.replace hashtbl char (Int32.succ cnt) )
-    char_seq ;
-  hashtbl
+  let arr = Array.make 26 0 in
+  let to_idx ch = int_of_char ch - int_of_char 'A' in
+  Seq.iter (fun ch -> arr.(to_idx ch) <- arr.(to_idx ch) + 1) char_seq ;
+  arr
 
-let find_max_freq_hashtbl hashtbl =
-  let hashtbl_list = List.of_seq (Hashtbl.to_seq hashtbl) in
-  let sorted =
-    List.sort (fun x y -> -Int32.compare (snd x) (snd y)) hashtbl_list
+let find_max_freq_array arr =
+  let to_char idx = char_of_int (idx + int_of_char 'A') in
+  let third (_, _, c) = c in
+  let res =
+    Array.fold_left
+      (fun (idx, max, answer) freq ->
+        let idx' = idx + 1 in
+        if max = freq then (idx', max, '?')
+        else if max < freq then (idx', freq, to_char idx)
+        else (idx', max, answer) )
+      (0, 0, ' ') arr
   in
-  match sorted with
-  | [] -> '?'
-  | [one] -> fst one
-  | one :: two :: _ -> if snd one = snd two then '?' else fst one
+  third res
 
 let solve () =
   let solve_helper str =
-    let freq_tbl = make_hashtbl str in
-    let answer = find_max_freq_hashtbl freq_tbl in
+    let freq_arr = make_array str in
+    let answer = find_max_freq_array freq_arr in
     print_char answer
   in
   Scanf.scanf "%s" solve_helper
