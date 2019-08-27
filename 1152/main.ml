f45b7aaf229be4c48e1566840e2da50b65a015bf
Sangwoo Joh <lambdasnow@gmail.com>
2019-08-27 14:56:26 +0900

    Solved 1152

---
+++ 1152/main.ml
@@ -0,0 +1,12 @@
+let count_words str =
+  let str = String.trim str in
+  let spaces = ref 0 in
+  let count_space ch = if ch = ' ' then incr spaces in
+  String.iter count_space str ;
+  if String.length str = 0 then 0 else !spaces + 1
+
+let solve () =
+  let input = read_line () in
+  print_int (count_words input)
+
+let () = solve ()
