f77bbc5eaf2e1d4b02cccc40f35922853356b4ba
Sangwoo Joh <lambdasnow@gmail.com>
2019-08-28 14:54:55 +0900

    Solved 10886

---
+++ 10886/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ocamlc -o main main.ml
---
+++ 10886/main.ml
@@ -0,0 +1,11 @@
+let cute_or_not n =
+  let cute = ref 0 in
+  for i = 0 to n - 1 do
+    Scanf.scanf "%d\n" (fun x -> if x = 1 then incr cute)
+  done ;
+  if !cute > n / 2 then print_string "Junhee is cute!"
+  else print_string "Junhee is not cute!"
+
+let solve () = Scanf.scanf "%d\n" (fun total -> cute_or_not total)
+
+let () = solve ()
