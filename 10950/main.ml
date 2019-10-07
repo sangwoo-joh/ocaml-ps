857ce82c383ec9881b23742689b9736036944b06
Sangwoo Joh <lambdasnow@gmail.com>
2019-10-07 19:43:50 +0900

    Solved 10950

---
+++ 10950/main.ml
@@ -0,0 +1,9 @@
+let rec a_plus_b i =
+  if i = 0 then ()
+  else (
+    Scanf.scanf " %d %d" (fun a b -> print_endline (string_of_int (a + b))) ;
+    a_plus_b (i - 1) )
+
+let solve () = Scanf.scanf "%d" (fun n -> a_plus_b n)
+
+let () = solve ()
