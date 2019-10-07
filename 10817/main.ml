5817d20a6432d80462855f12bdd8468608f3aadf
Sangwoo Joh <lambdasnow@gmail.com>
2019-10-07 19:43:43 +0900

    Solved 10817

---
+++ 10817/main.ml
@@ -0,0 +1,7 @@
+let solve () =
+  Scanf.scanf "%d %d %d" (fun a b c ->
+      let nums = [a; b; c] in
+      let sorted = List.sort Pervasives.compare nums in
+      print_int (List.nth sorted 1) )
+
+let () = solve ()
