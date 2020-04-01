3df0980c27f7c3798c9df285024c84494ffb9e2e
Sangwoo Joh <lambdasnow@gmail.com>
2020-04-01 16:53:24 +0900

    [1068] Remove unused code

--- 1068/main.ml
+++ 1068/main.ml
@@ -26,19 +26,13 @@ module Digraph = struct
 
   type t = S.t M.t
 
-  type vertex = M.key
-
-  type edge = vertex * vertex
-
   let empty : t = NodeMap.empty
 
   let mem_vertex g v = M.mem v g
 
   let mem_edge g v1 v2 = try S.mem v2 (M.find v1 g) with Not_found -> false
 
-  let unsafe_add_vertex g v = M.add v S.empty g
-
-  let add_vertex g v = if M.mem v g then g else unsafe_add_vertex g v
+  let add_vertex g v = if M.mem v g then g else M.add v S.empty g
 
   let unsafe_add_edge g v1 v2 = M.add v1 (S.add v2 (M.find v1 g)) g
 
@@ -64,9 +58,7 @@ module Digraph = struct
     else true
 
 
-  let iter_succ f g v = S.iter f (M.find_and_raise v g "[iter_succ]")
-
-  let fold_vertex f = M.fold (fun v _ -> f v)
+  let iter_succ ~f g v = S.iter f (M.find_and_raise v g "[iter_succ]")
 end
 
 module Dfs (G : module type of Digraph) = struct
@@ -80,7 +72,7 @@ module Dfs (G : module type of Digraph) = struct
       if not (Stack.is_empty frontier) then (
         let visit = Stack.pop frontier in
         let acc = f visit acc in
-        G.iter_succ push g visit ; loop acc )
+        G.iter_succ ~f:push g visit ; loop acc )
       else acc
     in
     push root ; loop init
