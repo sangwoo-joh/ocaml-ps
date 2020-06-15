365993ea3303f86965518efa9581ef4ec6670532
Sangwoo Joh <lambdasnow@gmail.com>
2020-06-15 18:04:42 +0900

    Solve 10798

---
+++ src/p10798.ml
@@ -0,0 +1,13 @@
+let solve () =
+  let inputs = ref [] in
+  for i = 1 to 5 do
+    inputs := read_line () :: !inputs
+  done ;
+  let inputs = List.rev !inputs in
+  let cur = ref 0 in
+  while !cur < 15 do
+    List.iter (fun w -> if !cur < String.length w then print_char w.[!cur]) inputs ;
+    incr cur
+  done
+
+let () = solve ()
