d941584e4b1f08f3937935f8ad5c0ceee5676c09
Sangwoo Joh <lambdasnow@gmail.com>
2020-10-19 18:07:58 +0900

    Solved 2447 - referenced

---
+++ src/p2447.ml
@@ -0,0 +1,20 @@
+module F = Format
+
+let rec star y x n arr =
+  if n = 1 then Bytes.set arr.(y) x '*'
+  else
+    for dy = 0 to 2 do
+      for dx = 0 to 2 do
+        if dx = 1 && dy = 1 then ()
+        else
+          let dn = n / 3 in
+          star (y + (dn * dy)) (x + (dn * dx)) dn arr
+      done
+    done
+
+
+let () =
+  Scanf.scanf "%d" (fun n ->
+      let lines = Array.init n (fun _ -> Bytes.make n ' ') in
+      star 0 0 n lines;
+      Array.iter (fun line -> F.printf "%s\n" (Bytes.to_string line)) lines)
