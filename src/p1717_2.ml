a90df77ee04a2cf35f8eddda68585c5f8339586e
Sangwoo Joh <lambdasnow@gmail.com>
2021-10-07 16:19:15 +0900

    Apply rank compression

--- src/p1717_2.ml
+++ src/p1717_2.ml
@@ -10,13 +10,20 @@ module DisjointSet = struct
   module Make (Elt : Elt) = struct
     type elt = Elt.t
 
-    type t = { data : (elt, elt) Hashtbl.t; size : int ref }
+    type t = {
+      data : (elt, elt) Hashtbl.t;
+      size : int ref;
+      rank : (elt, int) Hashtbl.t;
+    }
 
-    let empty = { data = Hashtbl.create 100; size = ref 0 }
+    let empty =
+      { data = Hashtbl.create 100; size = ref 0; rank = Hashtbl.create 100 }
+
 
     let make_set t elt =
       if not (Hashtbl.mem t.data elt) then (
         Hashtbl.add t.data elt elt;
+        Hashtbl.add t.rank elt 0;
         incr t.size)
 
 
@@ -29,13 +36,26 @@ module DisjointSet = struct
       Hashtbl.find t.data elt
 
 
-    let union t x y =
+    let rec union t x y =
+      let union_aux leaf trunk =
+        Hashtbl.replace t.data leaf trunk;
+        decr t.size
+      in
+
       let px = find t x in
       let py = find t y in
 
-      if px <> py then (
-        decr t.size;
-        Hashtbl.replace t.data px py)
+      if px = py then ()
+      else
+        (* rank compression *)
+        let rankx = Hashtbl.find t.rank px in
+        let ranky = Hashtbl.find t.rank py in
+
+        if rankx < ranky then union_aux px py
+        else if ranky > rankx then union_aux py px
+        else (
+          union_aux py px;
+          Hashtbl.replace t.rank px (rankx + 1))
 
 
     let disjoint t x y = find t x <> find t y
